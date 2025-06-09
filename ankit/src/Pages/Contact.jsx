ed3f0860905c5d7b766155f2d11b849c8af7a6e2
ankit7078 <av8537742@gmail.com>
2025-06-09 15:13:16 +0530

    hell

--- ankit/src/Pages/Contact.jsx
+++ ankit/src/Pages/Contact.jsx
@@ -9,7 +9,7 @@ const ContactSection = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://ankit-api.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, message }),
